c72814824e971219d2ab82fdf90996cfb4aa5e5c
Axel-Jacobsen <axelnj44@gmail.com>
2021-07-01 12:15:19 -0700

    update files_eq

--- files_eq.sh
+++ files_eq.sh
@@ -1,5 +1,19 @@
 #! /usr/bin/env sh
 
-./huff $1 >/dev/null
-cmp --silent out/out.txt $1 && (echo "success"; exit 0) || (echo "failure"; exit 1)
+if [[ -d $1 ]]; then
+	for f in "$1"/*; do
+		if [[ $f != *.pine ]]; then
+			./huff $f >/dev/null;
+			cmp --silent out/out.txt $f && (echo "$f success"; exit 0) || (echo "$f failure"; exit 1)
+		else
+			echo "$f skipped"
+		fi
+	done
+elif [[ -f $1 ]]; then
+	./huff $1 >/dev/null;
+	cmp --silent out/out.txt $f && (echo "$f success"; exit 0) || (echo "$f failure"; exit 1)
+else
+	echo "$1 is not valid"
+	exit 1
+fi
 
