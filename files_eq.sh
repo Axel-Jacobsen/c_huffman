4a496bbfb29d8e93c56ba9cb58ddd02e5965698d
Axel-Jacobsen <axelnj44@gmail.com>
2021-07-01 18:30:29 -0700

    change file output to color

--- files_eq.sh
+++ files_eq.sh
@@ -1,18 +1,21 @@
 #! /usr/bin/env sh
 
 all_eq=1;
+RED='\033[0;31m'
+GRN='\033[0;32m'
+NC='\033[0m'
 
 if [[ -d $1 ]]; then
 	for f in "$1"/*; do
 		if [[ $f != *.pine ]]; then
 			./huff $f >/dev/null;
-			cmp --silent out/out.txt $f && (echo "$f success"; exit 0) || (echo "$f failure"; exit 1)
+			cmp --silent out/out.txt $f && (echo "$GRN$f$NC";) || (echo "$RED$f$NC";)
 			all_eq=$all_eq && [[ ! $? ]];
 		fi
 	done
 elif [[ -f $1 ]]; then
 	./huff $1 >/dev/null;
-	cmp --silent out/out.txt $f && (echo "$f success"; exit 0) || (echo "$f failure"; exit 1)
+	cmp --silent out/out.txt $f && (echo "$GRN$f$NC";) || (echo "$RED$f$NC";)
 	all_eq=$all_eq && [[ ! $? ]];
 else
 	echo "$1 is not valid - must be file or directory"
