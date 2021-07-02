9fa24fdc769398bc286142560cc3571a506a519a
Axel-Jacobsen <axelnj44@gmail.com>
2021-07-01 17:56:17 -0700

    exit codes for files_eq

--- files_eq.sh
+++ files_eq.sh
@@ -1,19 +1,22 @@
 #! /usr/bin/env sh
 
+all_eq=1;
+
 if [[ -d $1 ]]; then
 	for f in "$1"/*; do
 		if [[ $f != *.pine ]]; then
 			./huff $f >/dev/null;
 			cmp --silent out/out.txt $f && (echo "$f success"; exit 0) || (echo "$f failure"; exit 1)
-		else
-			echo "$f skipped"
+			all_eq=$all_eq && [[ ! $? ]];
 		fi
 	done
 elif [[ -f $1 ]]; then
 	./huff $1 >/dev/null;
 	cmp --silent out/out.txt $f && (echo "$f success"; exit 0) || (echo "$f failure"; exit 1)
+	all_eq=$all_eq && [[ ! $? ]];
 else
-	echo "$1 is not valid"
+	echo "$1 is not valid - must be file or directory"
 	exit 1
 fi
 
+exit $all_eq
